ff7051e77ae2af68c1b3c41de5f579dd80ca6092
Tiago Faria <tiaghpf@ua.pt>
2018-06-04 21:39:24 +0100

    Fix format of strings on tables

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -10,11 +10,48 @@ register = template.Library()
 @register.filter(name='format_string')
 @stringfilter
 def format_string(value):
-    values = value.replace(baseEntity, '').replace(baseProperty, '').replace('_', ' ').split(' ')
+    value = value.replace(baseEntity, '').replace(baseProperty, '').replace('_', ' ')
+    if ' ' in value:
+        value = split_by_space(value)
+    if '/' in value:
+        value = split_by_bar(value)
+    if '-' in value:
+        value = split_by_hyphen(value)
+    if ' ' not in value and '/' not in value and '-' not in value:
+        value = value.title()
+    return value
+
+
+def split_by_space(value):
+    if ' ' in value:
+        words = value.split(' ')
+        res = ""
+        for word in words:
+            if word in areas or word in platforms:
+                res += word.upper() + ' '
+            else:
+                res += word.title() + ' '
+        return res.strip()
+    return value
+
+
+def split_by_bar(value):
+    words = value.split('/')
     res = ""
-    for i in values:
-        if i in areas or i in platforms:
-            res += i.upper() + ' '
+    for word in words:
+        if word in areas or word in platforms:
+            res += word.upper() + '/'
         else:
-            res += i.capitalize() + ' '
-    return res.strip()
+            res += word.title() + '/'
+    return res[:-1].strip()
+
+
+def split_by_hyphen(value):
+    words = value.split('-')
+    res = ""
+    for word in words:
+        if word in areas or word in platforms:
+            res += word.upper() + '-'
+        else:
+            res += word.title() + '-'
+    return res[:-1].strip()
